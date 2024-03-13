f3722b6353833d08245d8ca19b46d0c5dfa93cdc
lipsilverhand <williamhuynh99@gmail.com>
2024-03-13 16:08:20 -0700

    updated

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,17 +1,24 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const exec = require('@actions/exec');
-
+const { v4: uuidv4 } = require('uuid'); // Import the v4 method from the uuid module
 
 function run() {
-    const bucket = core.getInput('bucket', { required: true });
-    const bucketRegion = core.getInput(' bucket-region', { required: true });
-    const distFolder = core.getInput('dist-folder', { required: true });
+    try {
+        const bucket = core.getInput('bucket', { required: true });
+        const bucketRegion = core.getInput('bucket-region', { required: true });
+        const distFolder = core.getInput('dist-folder', { required: true });
 
-    const s3Uri = `s3://${bucket}`;
-    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+        const s3Uri = `s3://${bucket}`;
+        exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
-    core.notice('Hello from my custom Javascript Action!');
+        // Generate a UUID
+        const uuid = uuidv4();
+        core.info('Generated UUID:', uuid); // Log the generated UUID
+
+        core.info('Deployment completed successfully.');
+    } catch (error) {
+        core.setFailed(`Deployment failed with error: ${error.message}`);
+    }
 }
 
 run();
